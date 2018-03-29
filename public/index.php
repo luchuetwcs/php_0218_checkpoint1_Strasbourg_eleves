49353fb9428cc72053c3e17fc1589d4a5a31aa69
Cyril Bellamy <cyrilbellamy1@gmail.com>
2018-03-29 12:38:20 +0200

    Ajout du formulaire foncrtionnel pour rentrer en base de donnée

--- public/index.php
+++ public/index.php
@@ -3,16 +3,51 @@
 require '../connection/connect.php';
 $pdo = new PDO(DSN, USER, PASS);
 
-    //préparation de la requete de selection
-$querySelect = 'SELECT CONCAT(lastname, " ", firstname) as fullname, civility FROM contact JOIN civility civ ON civ.id = contact.civility_id';
-$prep = $pdo->query($querySelect);
+   
 
     //préparation de la requete d'insertion
-$queryInsert = "INSERT INTO contact (lastname, firstname, civility_id)    VALUES (:lastname, :firstname, :civility_id);";
+$queryInsert = 'INSERT INTO contact (lastname, firstname, civility_id)    VALUES (:lastname, :firstname, :civility);';
 $prep2 = $pdo->prepare($queryInsert);
 
 
+    // Test formulaire
+if ($_SERVER["REQUEST_METHOD"]==='POST') {
+      //analyse des données reçues en post
+      //si une donnée est reçue, on la lie à la préparation de l'Insertion
+      //sinon on rentre des données dans le tableau d'erreur
+      if(!isset($_POST['civility']) || empty($_POST['civility']))
+      {
+        $error['civility'] = "Enter civility.";
+      }else {
+        if ($_POST['civility']==1) {
+            $prep2->bindValue(':civility',1, PDO::PARAM_INT);
+        }else{
+            $prep2->bindValue(':civility',2, PDO::PARAM_INT);
+        }
+      }
 
+      if(!isset($_POST['firstname']) || empty($_POST['firstname']))
+      {
+        $error['firstname'] = "Enter firstname.";
+      }else {
+        $prep2->bindValue(':firstname',$_POST['firstname'], PDO::PARAM_STR);
+      }
+
+      if(!isset($_POST['lastname']) || empty($_POST['lastname']))
+      {
+        $error['lastname'] = "Enter lastname.";
+      }else {
+        $prep2->bindValue(':lastname',$_POST['lastname'], PDO::PARAM_STR);
+      }
+
+      //si il n'y a pas d'erreur, on execute la requete d'insertion préparée
+      if(empty($error))
+      {
+        $prep2->execute();
+      }else{
+          echo "Tout les champs ne sont pas rempli !!";
+      }
+    }
 
 
 ?>
@@ -22,7 +57,7 @@ $prep2 = $pdo->prepare($queryInsert);
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Contact</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -30,6 +65,24 @@ $prep2 = $pdo->prepare($queryInsert);
 </head>
 
 <body>
+    <div class="container">
+        <form action="" method="post">
+            <label for="civility"> Civility</label><br>
+            <select name="civility">
+                <option value="1" name="M">Mr.</option>
+                <option value="2" name="Mme">Mrs.</option>   
+            </select><br>
+            <label for="firstname"> Fistname</label><br>
+            <input type="text" name="firstname" placeholder="Firstname" value=""><br>
+            <label for="lastname"> lastname</label><br> 
+            <input type="text" name="lastname" placeholder="Lastname" value=""><br>
+            <br>
+            <button type="submit">Envoyer</button><br>
+        </form>
+    </div>
+<br>
+<hr>
+<br>
     <div class="container">
     <table class="table table-bordered">
         <thead>
@@ -40,8 +93,13 @@ $prep2 = $pdo->prepare($queryInsert);
         </thead>
         <tbody>
           <?php
+
+           //préparation de la requete de selection
+            $querySelect = 'SELECT CONCAT(lastname, " ", firstname) as fullname, civility FROM contact JOIN civility civ ON civ.id = contact.civility_id';
+            $prep = $pdo->query($querySelect);
           //on récupérer les données de l'exécution de la requete de selection
             $donnees = $prep->fetchAll();
+
 
           //on parcours le résultat pour les mettre dans la table
             foreach($donnees as $data)
--- src/functions.php
+++ src/functions.php
@@ -4,12 +4,7 @@
 $pdo = new PDO(DSN, USER, PASS);
 
 
-class contact
-{
-    private $querySelect = 'SELECT CONCAT(lastname, " ", firstname) as fullname, civility FROM contact JOIN civility civ ON civ.id = contact.civility_id';
-
-    public function getQuery(){
-        return $this->$querySelect;
+function fullname($fullname){
+    
+    
     }
-
-}
